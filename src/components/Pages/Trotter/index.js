c6d72d1b16c80305f16e0f17405786aebcc7265b
Helene <helenesauveparis@gmail.com>
2023-04-14 19:04:11 +0100

    creating json file and importing it in Trotter

--- src/components/Pages/Trotter/index.js
+++ src/components/Pages/Trotter/index.js
@@ -3,24 +3,11 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Icon } from "leaflet";
 import "./style.css";
 import "leaflet/dist/leaflet.css";
+import locations from "./locations.json";
 
 
 function Trotter() {
-  const markers = [
-    {
-      geocode: [48.860764, 2.393646],
-      popUp: "Cemetery of Père Lachaise, where Proust is buried",
-    },
-    {
-      geocode: [48.87559171099804, 2.318465364416511],
-      popUp: "This is where Proust was born on 10 July 1871",
-    },
-    {
-      geocode: [48.8709476111008, 2.322475093255008],
-      popUp:
-        "In 1873, the Proust family move into immeuble boulevard Malesherbes, dans lequel ils resteront jusqu’en 1900.",
-    },
-  ];
+  const markers = locations
 
   const customIcon = new Icon ({
     iconUrl: require('./pin.png'),
