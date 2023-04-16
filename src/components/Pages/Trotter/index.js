df4e9fb3d0cf68f7c9c43d98bd86495bc46720f3
Helene <helenesauveparis@gmail.com>
2023-04-16 19:02:00 +0100

    adding clusters

--- src/components/Pages/Trotter/index.js
+++ src/components/Pages/Trotter/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { Icon } from "leaflet";
+import { Icon, divIcon } from "leaflet";
 import "./style.css";
 import "leaflet/dist/leaflet.css";
 import locations from "./locations.json";
+import MarkerClusterGroup from "react-leaflet-cluster";
 
 
 function Trotter() {
@@ -14,18 +15,27 @@ function Trotter() {
     iconSize: [38, 38] // size of the icon
   })
 
+  const createCustomClusterIcon = (cluster) => {
+    return new divIcon ({
+        html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`
+    })
+  }
   return (
     <MapContainer center={[48.8566, 2.3522]} zoom={5}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-
+    <MarkerClusterGroup
+    chunkedLoading
+    iconCreateFunction={createCustomClusterIcon}
+    >
       {markers.map(marker => (
         <Marker position={marker.geocode} icon={customIcon}>
             <Popup>{marker.popUp}</Popup>
         </Marker>
       ))}
+    </MarkerClusterGroup>
     </MapContainer>
   );
 }
