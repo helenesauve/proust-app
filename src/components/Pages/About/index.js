15612445728a5e3ee8bac3a5bfbdd3655b9a88bf
Helene <helenesauveparis@gmail.com>
2023-04-28 16:33:57 +0100

    rewriting locations.json

--- src/components/Pages/About/index.js
+++ src/components/Pages/About/index.js
@@ -7,7 +7,7 @@ function About () {
         <div className="container">
             <p>This is an app for Marcel Proust lovers around the world.</p> 
             <p>It is intended to incorporate elements of the books in everyday life, provide an overview of places that Marcel Proust visited or that are mentioned in the books, and offer general entertainment relating to the writer.</p>
-            <p>This is a work in progress, keep an eye out for updated and get in touch at <a href="mailto:helenecatherinesauve@gmail.com">helenecatherinesauve@gmail.com</a> for any recommendations.</p>
+            <p>This is a work in progress, keep an eye out for updated content and get in touch at <a href="mailto:helenecatherinesauve@gmail.com">helenecatherinesauve@gmail.com</a> for any recommendations.</p>
             </div>
     )
 }
